2c25d62a838d9835637f285b813090899cd8b41d
Nicholas Chung <iamnicholaschung@gmail.com>
2021-06-13 21:21:08 +0800

    Improve css for buttons

--- src/Containers/Home.tsx
+++ src/Containers/Home.tsx
@@ -1,18 +1,27 @@
 import React from "react";
 import "./Home.css";
 import logo from "../../src/logo.svg";
+import { Box, Button, Container } from "@material-ui/core";
 
 const Home = () => {
 	return (
-		<div className="Home-border">
-			<div className="Home-align">
-				<img src={logo} className="Home-logo" alt="LOGO" />
-				<h2>ANALYTICAL REPORT</h2>
-				<p>Summarised Reports at your fingertips.</p>
-				<button>REGISTER</button>
-				<button>LOGIN</button>
+		<Container maxWidth="sm">
+			<div className="home-border">
+				<div className="home-align">
+					<img src={logo} className="home-logo" alt="LOGO" />
+					<h2>ANALYTICAL REPORT</h2>
+					<p>Summarised Reports at your fingertips.</p>
+					<Box component="span" m={1} className="home-button-container">
+						<Button variant="contained" color="secondary">
+							REGISTER
+						</Button>
+						<Button variant="contained" color="primary">
+							LOGIN
+						</Button>
+					</Box>
+				</div>
 			</div>
-		</div>
+		</Container>
 	);
 };
 
