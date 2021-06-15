aa7a827ba27ce5eab7ca74c5c80b1a856f5b3f1c
Nicholas Chung <iamnicholaschung@gmail.com>
2021-06-15 20:14:09 +0800

    Add register component

--- src/Components/Register.tsx
+++ src/Components/Register.tsx
@@ -1,42 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Register.css";
 import "./Shared/Card.css";
 import { Container } from "@material-ui/core";
-import { Card, Button, FormControl, Input, InputLabel, FormHelperText } from "@material-ui/core";
+import { Card, Button, FormControl, Input, InputLabel, FormHelperText, Snackbar } from "@material-ui/core";
+import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
+interface IRegister {
+	email: string;
+	password: string;
+	firstName: string;
+	lastname: string;
+}
 const Register = () => {
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm();
+
+	const [open, setOpen] = useState(false);
+
+	const handleClose = () => {
+		setOpen(false);
+	};
+
+	const onSubmit = (data: IRegister) => setOpen(true);
 	return (
 		<Container maxWidth="sm">
+			<Snackbar
+				anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+				open={open}
+				onClose={handleClose}
+				autoHideDuration={3000}
+				message="Registration Successful"
+				key="snackbar"
+			/>
 			<Card className="card" variant="outlined">
 				<div className="card-container">
 					<h2>REGISTER</h2>
-					<FormControl>
-						<InputLabel>Email</InputLabel>
-						<Input aria-label="email" type="text" placeholder="email" />
-					</FormControl>
-					<FormControl>
-						<InputLabel>Password</InputLabel>
-						<Input aria-label="password" type="text" placeholder="password" />
-						<FormHelperText id="password-helper">
-							Do remember to include : <br />
-							<span className="register-helper">
-								1 Uppercase, <br />1 Lowercase <br />
-								and numbers
-							</span>
-						</FormHelperText>
-					</FormControl>
-					<FormControl>
-						<InputLabel>First Name</InputLabel>
-						<Input aria-label="firstName" type="text" placeholder="firstName" />
-					</FormControl>
-					<FormControl>
-						<InputLabel>Last Name</InputLabel>
-						<Input aria-label="lastName" type="text" placeholder="lastName" />
-					</FormControl>
+					<form className="card-container" onSubmit={handleSubmit(onSubmit)}>
+						<FormControl size="small">
+							<InputLabel>Email</InputLabel>
+							<Input {...register("lastName", { required: true })} aria-label="email" type="text" placeholder="Email" />
+							{errors.email && <p>Email is required.</p>}
+						</FormControl>
+						<FormControl>
+							<InputLabel>Password</InputLabel>
+							<Input
+								{...register("password", { required: true })}
+								aria-label="password"
+								type="password"
+								placeholder="Password"
+							/>
+							{errors.password && <p>Password is required.</p>}
+
+							<FormHelperText id="password-helper">
+								Do remember to include : <br />
+								<span className="register-helper">
+									1 Uppercase, <br />1 Lowercase <br />
+									and numbers
+								</span>
+							</FormHelperText>
+						</FormControl>
+						<FormControl>
+							<InputLabel>First Name</InputLabel>
+							<Input {...register("firstName")} aria-label="firstName" type="text" placeholder="First Name" />
+							{errors.lastName && <p>Last name is required.</p>}
+						</FormControl>
+						<FormControl>
+							<InputLabel>Last Name</InputLabel>
+							<Input
+								{...register("lastName", { required: true })}
+								aria-label="lastName"
+								type="text"
+								placeholder="Last Name"
+							/>
+						</FormControl>
+						<Input className="submit-button" type="submit" />
+					</form>
 					<div className="register-container">
-						<Button variant="contained" className="button" color="primary">
-							SUBMIT
-						</Button>
 						<Link to="/" className="button">
 							<Button variant="contained" color="secondary">
 								Back to Home
