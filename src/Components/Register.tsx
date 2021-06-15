180e9ef76c641f4881e9de706988bd6a13a2e1b5
Nicholas Chung <iamnicholaschung@gmail.com>
2021-06-15 18:43:05 +0800

    Update css and split css for shared components

--- src/Components/Register.tsx
+++ src/Components/Register.tsx
@@ -1,17 +1,50 @@
 import React from "react";
 import "./Register.css";
-import logo from "../../src/logo.svg";
+import "./Shared/Card.css";
 import { Container } from "@material-ui/core";
-
+import { Card, Button, FormControl, Input, InputLabel, FormHelperText } from "@material-ui/core";
+import { Link } from "react-router-dom";
 const Register = () => {
 	return (
 		<Container maxWidth="sm">
-			<div className="home-border">
-				<div className="home-align">
-					<img src={logo} className="home-logo" alt="LOGO" />
+			<Card className="card" variant="outlined">
+				<div className="card-container">
 					<h2>REGISTER</h2>
+					<FormControl>
+						<InputLabel>Email</InputLabel>
+						<Input aria-label="email" type="text" placeholder="email" />
+					</FormControl>
+					<FormControl>
+						<InputLabel>Password</InputLabel>
+						<Input aria-label="password" type="text" placeholder="password" />
+						<FormHelperText id="password-helper">
+							Do remember to include : <br />
+							<span className="register-helper">
+								1 Uppercase, <br />1 Lowercase <br />
+								and numbers
+							</span>
+						</FormHelperText>
+					</FormControl>
+					<FormControl>
+						<InputLabel>First Name</InputLabel>
+						<Input aria-label="firstName" type="text" placeholder="firstName" />
+					</FormControl>
+					<FormControl>
+						<InputLabel>Last Name</InputLabel>
+						<Input aria-label="lastName" type="text" placeholder="lastName" />
+					</FormControl>
+					<div className="register-container">
+						<Button variant="contained" className="button" color="primary">
+							SUBMIT
+						</Button>
+						<Link to="/" className="button">
+							<Button variant="contained" color="secondary">
+								Back to Home
+							</Button>
+						</Link>
+					</div>
 				</div>
-			</div>
+			</Card>
 		</Container>
 	);
 };
--- src/Containers/Home.tsx
+++ src/Containers/Home.tsx
@@ -1,32 +1,33 @@
 import React from "react";
 import "./Home.css";
-import logo from "../../src/logo.svg";
-import { Box, Button, Container } from "@material-ui/core";
-import { Link } from "react-router-dom";
 import "../Components/Shared/Button.css";
+import "../Components/Shared/Card.css";
+import logo from "../../src/logo.svg";
+import { Card, Box, Button, Container } from "@material-ui/core";
+import { Link } from "react-router-dom";
 
 const Home = () => {
 	return (
 		<Container maxWidth="sm">
-			<div className="home-border">
-				<div className="home-align">
+			<Card className="card">
+				<div className="card-container">
 					<img src={logo} className="home-logo" alt="LOGO" />
 					<h2>ANALYTICAL REPORT</h2>
 					<p>Summarised Reports at your fingertips.</p>
 					<Box component="span" m={1} className="home-button-container">
-						<Link to="/register" className="button-link">
+						<Link to="/register" className="button">
 							<Button variant="contained" color="secondary">
 								REGISTER
 							</Button>
 						</Link>
-						<Link to="/login" className="button-link">
+						<Link to="/login" className="button">
 							<Button variant="contained" color="primary">
 								LOGIN
 							</Button>
 						</Link>
 					</Box>
 				</div>
-			</div>
+			</Card>
 		</Container>
 	);
 };
